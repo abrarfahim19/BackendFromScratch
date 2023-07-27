c9241db2b2c17e987d6b7e61d9711680ead31383
abrarfahim19 <fahimpatwari19@gmail.com>
2023-07-27 10:38:56 +0600

    Part 2

---
+++ Build your own Express/server.js
@@ -0,0 +1,8 @@
+const minimal = require("./src/minimal");
+
+const CONFIG = require("./config");
+
+const app = minimal();
+const server = app.listen(CONFIG.PORT, () =>
+  console.log(`Server running on ${CONFIG.PORT}`)
+);
